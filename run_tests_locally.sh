7665cee240d0bd9e60721c95dacf36b7f67547a8
HFRibeiro <ribeirh02@gmail.com>
2024-10-11 15:27:00 +0100

    CT-1375: Updated test

--- run_tests_locally.sh
+++ run_tests_locally.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
-docker run -it --rm \
-  -v "$(pwd):/build" \
-  -w /build \
-  artefact.skao.int/ska-cicd-k8s-tools-build-deploy:0.13.4 \
-  bash -c "
-    export POETRY_CONFIG_VIRTUALENVS_CREATE='false';
-    export PIP_CACHE_DIR='./.cache/pip';
+# Define the Python versions to test
+PYTHON_VERSIONS=("3.10") # "3.11" "3.12"
 
-    if [ -f .make/python.mk ]; then
-      echo '.make/python.mk found.';
-    else
-      echo 'File python.mk not included in Makefile; exiting.';
-      exit 1;
-    fi
+for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
+  echo "Running tests with Python $PYTHON_VERSION..."
 
-    make help | grep python-test;
+  docker run -it --rm \
+    -v "$(pwd):/build" \
+    -w /build \
+    ghcr.io/astral-sh/uv:python${PYTHON_VERSION}-bookworm \
+    bash -c "
+      # Change directory to tangogql-ariadne and perform setup as in 'before_script'
+      cd tangogql-ariadne/;
+      ls -a;
+      apt update;
+      apt install -y jq \$EXTRA_DEB_PACKAGES;
+      python -m venv /tools;
+      /tools/bin/python -m pip install wheel-inspect;
+      
+      # Install pytest and other test dependencies
+      pip install pytest-cov pytest-forked;
+      pip install -e '.[tests]';
 
-    poetry config virtualenvs.in-project true;
-    echo 'python-build Setting.. poetry config virtualenvs.create \$POETRY_CONFIG_VIRTUALENVS_CREATE';
-    poetry config virtualenvs.create \$POETRY_CONFIG_VIRTUALENVS_CREATE;
+      # Run pytest with coverage options
+      pytest --cov=tangogql --cov-branch --cov-report term-missing --cov-report html --junitxml=report.xml \$PYTEST_EXTRA_ARGS;
+    "
 
-    if [[ -f pyproject.toml ]]; then
-      if [[ -n \$CI_POETRY_VERSION ]] && [[ \$(poetry --version) != *\$CI_POETRY_VERSION* ]]; then
-        echo 'python-test: Updating poetry to \$CI_POETRY_VERSION';
-        time poetry self update \$CI_POETRY_VERSION;
-      fi
-      echo 'python-test: Installing with poetry';
-      time poetry install --all-extras;
-    elif [[ -f requirements.txt ]]; then
-      echo 'python-test: Installing with pip';
-      time pip3 install -r requirements.txt;
-    fi
-
-    make python-test;
-  "
+  echo "Finished tests with Python $PYTHON_VERSION."
+done
